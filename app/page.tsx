fd9df6e3d48b505d3f64aa0c8321beed59ff9637
大林 正樹 <m-obayashi@oobayashinonotobukkukonpyuta.local>
2024-09-04 14:32:16 +0900

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -1,153 +1,157 @@
-"use client"
+"use client";
 
-import * as React from 'react'
+import * as React from "react";
 import {
-  flexRender,
-  getCoreRowModel,
-  getSortedRowModel,
-  useReactTable,
-  createColumnHelper,
-} from '@tanstack/react-table'
-import './index.css'
-import { faker } from '@faker-js/faker'
+	flexRender,
+	getCoreRowModel,
+	getSortedRowModel,
+	useReactTable,
+	createColumnHelper,
+} from "@tanstack/react-table";
+import "./index.css";
+import { faker } from "@faker-js/faker";
 
 declare module "@tanstack/table-core" {
-  // @ts-expect-error
-  interface ColumnMeta<TData extends RowData, TValue> {
-    rowSpan?: string;
-  }
+	// @ts-expect-error
+	interface ColumnMeta<TData extends RowData, TValue> {
+		rowSpan?: string;
+	}
 }
 
 type Test = {
-  id: number
-  pendingTransfer: number
-  amount: number
-}
+	id: number;
+	pendingTransfer: number;
+	amount: number;
+};
 
 const defaultData: Test[] = Array.from({ length: 30 }, () => ({
-  id: faker.number.int({ min: 0, max: 100 }),
-  pendingTransfer: faker.number.int({ min: 0, max: 100 }),
-  amount: faker.number.int({ min: 0, max: 100 }),
-}))
+	id: faker.number.int({ min: 0, max: 100 }),
+	pendingTransfer: faker.number.int({ min: 0, max: 100 }),
+	amount: faker.number.int({ min: 0, max: 100 }),
+}));
 
-
-const columnHelper = createColumnHelper<Test>()
+const columnHelper = createColumnHelper<Test>();
 
 const columns = [
-  columnHelper.accessor("amount", {
-    id: 'id',
-    cell: info => info.getValue(),
-    header: () => "ID",
-  }),
-  columnHelper.group({
-    id: '数量',
-    header: () => <span>数量</span>,
-    columns: [
-      columnHelper.accessor(row => row.amount, {
-        id: 'amount',
-        cell: info => info.getValue(),
-        header: () => <span>保有数量</span>,
-      }),
-      columnHelper.accessor('pendingTransfer', {
-        id: 'pendingTransfer',
-        header: () => <span>内移転中数量</span>,
-        cell: info => info.getValue(),
-      }),
-    ],
-  }),
-]
+	columnHelper.accessor("id", {
+		id: "id",
+		cell: (info) => info.getValue(),
+		header: () => "ID",
+	}),
+	columnHelper.group({
+		id: "数量",
+		header: () => <span>数量</span>,
+		columns: [
+			columnHelper.accessor((row) => row.amount, {
+				id: "amount",
+				cell: (info) => info.getValue(),
+				header: () => <span>保有数量</span>,
+			}),
+			columnHelper.accessor("pendingTransfer", {
+				id: "pendingTransfer",
+				header: () => <span>内移転中数量</span>,
+				cell: (info) => info.getValue(),
+			}),
+		],
+	}),
+];
 
 export const Home = () => {
-  const [data, _setData] = React.useState(() => [...defaultData])
-  const rerender = React.useReducer(() => ({}), {})[1]
+	const [data, _setData] = React.useState(() => [...defaultData]);
+	const rerender = React.useReducer(() => ({}), {})[1];
 
-  const table = useReactTable({
-    columns,
-    data,
-    debugTable: true,
-    getCoreRowModel: getCoreRowModel(),
-    getSortedRowModel: getSortedRowModel(),
-  })
+	const table = useReactTable({
+		columns,
+		data,
+		debugTable: true,
+		getCoreRowModel: getCoreRowModel(),
+		getSortedRowModel: getSortedRowModel(),
+	});
 
-  return (
-    <div className="p-2">
-      <div className="h-2" />
-      <table>
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                const rowSpan = header.column.columnDef.meta?.rowSpan;
+	table.getHeaderGroups().map((header) => {
+		console.log(header);
+	});
 
-                if (
-                  !header.isPlaceholder &&
-                  rowSpan !== undefined &&
-                  header.id === header.column.id
-                ) {
-                  return null;
-                }
+	return (
+		<div className="p-2">
+			<div className="h-2" />
+			<table>
+				<thead>
+					{table.getHeaderGroups().map((headerGroup) => (
+						<tr key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								const rowSpan = header.column.columnDef.meta?.rowSpan;
 
-                return (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder ? null : (
-                      <div
-                        className={
-                          header.column.getCanSort()
-                            ? 'cursor-pointer select-none'
-                            : ''
-                        }
-                        onClick={header.column.getToggleSortingHandler()}
-                        title={
-                          header.column.getCanSort()
-                            ? header.column.getNextSortingOrder() === 'asc'
-                              ? 'Sort ascending'
-                              : header.column.getNextSortingOrder() === 'desc'
-                                ? 'Sort descending'
-                                : 'Clear sort'
-                            : undefined
-                        }
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                        {{
-                          asc: ' 🔼',
-                          desc: ' 🔽',
-                        }[header.column.getIsSorted() as string] ?? null}
-                      </div>
-                    )}
-                  </th>
-                )
-              })}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table
-            .getRowModel()
-            .rows.slice(0, 10)
-            .map(row => {
-              return (
-                <tr key={row.id}>
-                  {row.getVisibleCells().map(cell => {
-                    return (
-                      <td key={cell.id}>
-                        {flexRender(
-                          cell.column.columnDef.cell,
-                          cell.getContext()
-                        )}
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
-      <div>{table.getRowModel().rows.length.toLocaleString()} Rows</div>
-    </div>
-  )
-}
+								if (
+									!header.isPlaceholder &&
+									rowSpan !== undefined &&
+									header.id === header.column.id
+								) {
+									return null;
+								}
 
-export default Home
+								return (
+									<th key={header.id} colSpan={header.colSpan}>
+										{header.isPlaceholder ? null : (
+											// biome-ignore lint:
+											<div
+												className={
+													header.column.getCanSort()
+														? "cursor-pointer select-none"
+														: ""
+												}
+												onClick={header.column.getToggleSortingHandler()}
+												title={
+													header.column.getCanSort()
+														? header.column.getNextSortingOrder() === "asc"
+															? "Sort ascending"
+															: header.column.getNextSortingOrder() === "desc"
+																? "Sort descending"
+																: "Clear sort"
+														: undefined
+												}
+											>
+												{flexRender(
+													header.column.columnDef.header,
+													header.getContext(),
+												)}
+												{{
+													asc: " 🔼",
+													desc: " 🔽",
+												}[header.column.getIsSorted() as string] ?? null}
+											</div>
+										)}
+									</th>
+								);
+							})}
+						</tr>
+					))}
+				</thead>
+				<tbody>
+					{table
+						.getRowModel()
+						.rows.slice(0, 10)
+						.map((row) => {
+							return (
+								<tr key={row.id}>
+									{row.getVisibleCells().map((cell) => {
+										return (
+											<td key={cell.id}>
+												{flexRender(
+													cell.column.columnDef.cell,
+													cell.getContext(),
+												)}
+											</td>
+										);
+									})}
+								</tr>
+							);
+						})}
+				</tbody>
+			</table>
+			<div>{table.getRowModel().rows.length.toLocaleString()} Rows</div>
+		</div>
+	);
+};
+
+export default Home;
